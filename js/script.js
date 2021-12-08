019922c5cca70369762276aa0ade0edff5a09335
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-08 23:43:16 +0600

    Login form added, Navbar functions are responsive with media queries

--- js/script.js
+++ js/script.js
@@ -16,7 +16,24 @@ document.querySelector('#menu-btn').onclick = () => {
 
 let cart = document.querySelector('.shopping-cart');
 
-document.querySelector('#cart-btn').onclick = () =>{
-    closer.style.display = 'block';
-    cart.classList.toggle('active');
+document.querySelector('#cart-btn').onclick = () => {
+  closer.style.display = 'block';
+  cart.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+  closer.style.display = 'block';
+  loginForm.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  searchForm.classList.remove('active');
 }
